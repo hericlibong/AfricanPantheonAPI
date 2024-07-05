82b4541dded4ac6da4c76acd730016e5850787b6
hericlibong <hericlibong@gmail.com>
2024-07-05 15:36:00 +0200

    added mixin for details viewsets

--- src/entities/views.py
+++ src/entities/views.py
@@ -1,74 +1,49 @@
 from rest_framework.viewsets import ModelViewSet
 from .models import Divinity, Category, Hero, MythicalCreature
-from .serializers import DivinityDetailSerializer, DivinityListSerializer, CategoryDetailSerializer, CategoryListSerializer, HeroDetailSerializer, HeroListSerializer, MythicalCreatureDetailSerializer, MythicalCreatureListSerializer
+from .serializers import (DivinityDetailSerializer, DivinityListSerializer, CategoryDetailSerializer, 
+                          CategoryListSerializer, HeroDetailSerializer, HeroListSerializer, 
+                          MythicalCreatureDetailSerializer, MythicalCreatureListSerializer)
 
 from django_filters.rest_framework import DjangoFilterBackend
 from .filters import DivinityFilter, HeroFilter, MythicalCreatureFilter
 
+class MultipleSerializerMixin:
+    detail_serializer_class = None
+
+    def get_serializer_class(self):
+        if self.action == 'retrieve' and self.detail_serializer_class is not None:
+            return self.detail_serializer_class
+        return self.serializer_class
 
 
-class CategoryViewSet(ModelViewSet):
+class CategoryViewSet(MultipleSerializerMixin, ModelViewSet):
+    queryset = Category.objects.all()
     serializer_class = CategoryListSerializer
     detail_serializer_class = CategoryDetailSerializer
     filter_backends = [DjangoFilterBackend]
     
-    def get_queryset(self):
-        return Category.objects.all()
     
-    def get_serializer_class(self):
-        if self.action == 'retrieve':
-            return self.detail_serializer_class
-        return self.serializer_class
     
-
-    
-
-
-class DivinityViewSet(ModelViewSet):
+class DivinityViewSet(MultipleSerializerMixin, ModelViewSet):
+    queryset = Divinity.objects.all()
     serializer_class = DivinityListSerializer
     detail_serializer_class = DivinityDetailSerializer
-    
-    def get_queryset(self):
-        return Divinity.objects.all()
-    
-    def get_serializer_class(self):
-        if self.action == 'retrieve':
-            return self.detail_serializer_class
-        return super().get_serializer_class() 
-    
     filter_backends= [DjangoFilterBackend]
     filterset_class = DivinityFilter
 
 
-class HeroViewSet(ModelViewSet):
+class HeroViewSet(MultipleSerializerMixin, ModelViewSet):
+   queryset = Hero.objects.all()
    serializer_class = HeroListSerializer
    detail_serializer_class = HeroDetailSerializer
-
-   def get_queryset(self):
-       return Hero.objects.all()
-   
-   def get_serializer_class(self):
-        if self.action == 'retrieve':
-            return self.detail_serializer_class
-        return super().get_serializer_class() 
-    
    filter_backends = [DjangoFilterBackend]
    filterset_class = HeroFilter
 
 
-class MythicalCreatureViewSet(ModelViewSet):
+class MythicalCreatureViewSet(MultipleSerializerMixin, ModelViewSet):
+    queryset = MythicalCreature.objects.all()
     serializer_class = MythicalCreatureListSerializer
-    detail_serializer_class = MythicalCreatureDetailSerializer
-    
-    def get_queryset(self):
-        return MythicalCreature.objects.all()
-    
-    def get_serializer_class(self):
-        if self.action == 'retrieve':
-            return self.detail_serializer_class
-        return super().get_serializer_class() 
-    
-        
+    detail_serializer_class = MythicalCreatureDetailSerializer  
     filter_backends = [DjangoFilterBackend]
     filterSet = MythicalCreatureFilter
     
