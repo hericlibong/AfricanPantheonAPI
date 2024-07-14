159f8c5bd894dcebe99f877d355446ef63864534
hericlibong <hericlibong@gmail.com>
2024-07-14 15:51:12 +0200

    added documentation to views

--- src/entities/views.py
+++ src/entities/views.py
@@ -3,33 +3,40 @@ from .models import Divinity, Category, Hero, MythicalCreature
 from .serializers import (DivinityDetailSerializer, DivinityListSerializer, CategoryDetailSerializer, 
                           CategoryListSerializer, HeroDetailSerializer, HeroListSerializer, 
                           MythicalCreatureDetailSerializer, MythicalCreatureListSerializer)
-
 from django_filters.rest_framework import DjangoFilterBackend
 from .filters import DivinityFilter, HeroFilter, MythicalCreatureFilter
 
+
 class MultipleSerializerMixin:
+    """A mixin that allows you to have different serializers for different actions."""
     detail_serializer_class = None
 
     def get_serializer_class(self):
+        """Return the class to use for the serializer."""
         if self.action == 'retrieve' and self.detail_serializer_class is not None:
             return self.detail_serializer_class
         return self.serializer_class
 
 
 class AdminCategoryViewSet(MultipleSerializerMixin, ModelViewSet):
+    """A viewset for viewing and editing category instances."""
     queryset = Category.objects.all()
     serializer_class = CategoryListSerializer
     detail_serializer_class = CategoryDetailSerializer
     filter_backends = [DjangoFilterBackend]
 
+
 class AdminDivinityViewSet(MultipleSerializerMixin, ModelViewSet):
+    """A viewset for viewing and editing divinity instances."""
     queryset = Divinity.objects.all()
     serializer_class = DivinityDetailSerializer
     detail_serializer_class = DivinityDetailSerializer
     filter_backends = [DjangoFilterBackend]
     filterset_class = DivinityFilter
 
+
 class AdminHeroViewSet(MultipleSerializerMixin, ModelViewSet):
+    """A viewset for viewing and editing hero instances."""
     queryset = Hero.objects.all()
     serializer_class = HeroListSerializer
     detail_serializer_class = HeroDetailSerializer
@@ -38,14 +45,15 @@ class AdminHeroViewSet(MultipleSerializerMixin, ModelViewSet):
 
 
 class CategoryViewSet(MultipleSerializerMixin, ReadOnlyModelViewSet):
+    """A viewset for viewing category instances."""
     queryset = Category.objects.all()
     serializer_class = CategoryListSerializer
     detail_serializer_class = CategoryDetailSerializer
     filter_backends = [DjangoFilterBackend]
     
-    
-    
+       
 class DivinityViewSet(MultipleSerializerMixin, ReadOnlyModelViewSet):
+    """A viewset for viewing divinity instances."""
     queryset = Divinity.objects.all()
     serializer_class = DivinityListSerializer
     detail_serializer_class = DivinityDetailSerializer
@@ -54,6 +62,7 @@ class DivinityViewSet(MultipleSerializerMixin, ReadOnlyModelViewSet):
 
 
 class HeroViewSet(MultipleSerializerMixin, ReadOnlyModelViewSet):
+   """A viewset for viewing hero instances."""
    queryset = Hero.objects.all()
    serializer_class = HeroListSerializer
    detail_serializer_class = HeroDetailSerializer
@@ -62,6 +71,7 @@ class HeroViewSet(MultipleSerializerMixin, ReadOnlyModelViewSet):
 
 
 class MythicalCreatureViewSet(MultipleSerializerMixin, ReadOnlyModelViewSet):
+    """A viewset for viewing mythical creature instances."""
     queryset = MythicalCreature.objects.all()
     serializer_class = MythicalCreatureListSerializer
     detail_serializer_class = MythicalCreatureDetailSerializer  
